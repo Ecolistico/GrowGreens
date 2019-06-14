7607623a8ecabdc87af28f25bec7e344371297cf
jmcasimar <chemanuel_95@hotmail.com>
2019-06-14 09:48:29 -0500

    Python multiThread - WiFi or AP checked and automatically created an AP

--- Python/sysRasp/testThread.py
+++ Python/sysRasp/testThread.py
@@ -2,42 +2,53 @@ import threading
 import time
 import sysRasp
 
+WiFiState = 0
 WiFiTime = time.time()
-TocaniTime = time.time()
-number = 1
+WiFiCount = 0
 
-def calc_square(number):
-    print('Square: ', number * number)
-    
-def calc_quad(number):
-    print('Quad: ', number * number * number * number)
+TocaniTime = time.time()
 
 def check_WiFi():
+    global WiFiState
     global WiFiTime
-    if(time.time()- WiFiTime > 60):
-        if(sysRasp.isWiFi()): print("You are connected to WLAN")
-        elif(isAP()): print("You has an AP")
-        else: print("Need to setup an AP")
-        WiFiTime = time.time()
+    global WiFiCount
+    while True:
+        if(time.time()- WiFiTime > 20):
+            if(sysRasp.isWiFi()):
+                WiFiState = 1
+                WiFiCount = 0
+            elif(sysRasp.isAP()):
+                WiFiState = 2
+                WiFiCount = 0
+            else:
+                WiFiState = 0
+                WiFiCount += 1
+            WiFiTime = time.time()
+            
+        if(WiFiCount>=5):
+            WiFiCount = 0
+            print("Configuring AP...")
+            sysRasp.runShellCommand('sudo python APconfig.py')
 
 def tocani():
     global TocaniTime
-    global number
-     
-    if(time.time()- TocaniTime > 10):
-        calc_square(number)
-        calc_quad(number)
-        TocaniTime = time.time()
-        number += 1
-    
+    while True:
+        if(time.time()- TocaniTime > 10):
+            TocaniTime = time.time()
+            print("Tocani has to move")
+        
 if __name__ == "__main__":
     while True:
         thread1 = threading.Thread(target=check_WiFi, args=())
         thread2 = threading.Thread(target=tocani, args=())
-        
+
         thread1.start()
         thread2.start()
         
         thread1.join()
         thread2.join()
+        
+        while True:
+            pass
+        
         
