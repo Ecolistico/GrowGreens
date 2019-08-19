c089fcbb5119951e50031772710ee8fc4674258f
jmcasimar <chemanuel_95@hotmail.com>
2019-08-19 09:10:36 -0500

    Grow-Master setup finished

---
+++ Python/Master/setup.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+import os
+import sys
+import time
+import getpass
+import webbrowser
+sys.path.insert(0, './src/')
+from sysRasp import runShellCommand, isWiFi, getGatewayIPaddr
+
+# define our clear function 
+def clear(): 
+    # for windows 
+    if os.name == 'nt': 
+        _ = os.system('cls') 
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = os.system('clear')
+
+# define our ping function
+def ping():
+    if (os.name == 'nt'): # for windows
+        pg = "ping -n 1"
+    else : # for mac and linux(here, os.name is 'posix') 
+        pg = "ping -c 1"
+    return pg
+    
+config = False
+reconfig = ""
+attemp = 0
+WiFi = True
+staticIP = ""
+
+print("\033[1;32;40mConfigurando Grow Greens")
+print("\033[0;37;40mRevisando configuración WiFi...")
+time.sleep(2)
+if(not isWiFi()): # Checking WiFi status
+    WiFi = False
+    print("No se detecto ninguna red WiFi por favor configurela antes de continuar")
+    input("Presione enter para continuar")
+
+if(WiFi or isWiFi()): # Check if previous configuration files exists
+    print("Revisando configuraciones previas...")
+    time.sleep(2)
+    if(os.path.isfile("config.json")):
+        print("El sistema ya había sido previamente configurado, si lo haces nuevamente se perdera la configuración" +
+              " anterior.")
+        reconfig = input("¿Deseas continuar? s/n\n")
+        if(reconfig.startswith("s") or reconfig.startswith("S")):
+            while(attemp<3):
+                password = getpass.getpass()
+                if(password == "yomero"):
+                    attemp = 10
+                    runShellCommand('sudo rm /var/www/growgreens-setup.com/public/data/config.json')
+                    config = True
+                else:
+                    attemp += 1
+    else:
+        config = True
+        
+    if(config): # Open browser to config system
+        print("A continuación, se abrirá una página web solicitando información acerca de su ubicación" +
+              " actual. Por favor siga las indicaciones y después regrese a está ventana.")
+        time.sleep(3.5)
+        webbrowser.open('http://growgreens-setup.com')  # Go to growgreens-setup.com
+        while(not os.path.isfile("/var/www/growgreens-setup.com/public/data/config.json")):
+            pass
+        clear()
+        runShellCommand('sudo cp /var/www/growgreens-setup.com/public/data/config.json config.json')
+        print("La ubicación fue configurada correctamente")
+        
+        static = input("¿Es el primer sistema que se configura en la bodega? s/n\n")
+        # If we do not have an MQTT Broker we need to set an static IP
+        if(static.startswith("s") or static.startswith("s")):
+            Gateway_IP = getGatewayIPaddr()
+            IP_array = Gateway_IP.split('.')
+            red = IP_array[0]+"."+IP_array[1]+"."+IP_array[2]+"."
+        
+            # Check availables IP
+            print("A continuación se realizará un escaneo de la red WiFi")
+            print("[*] El escaneo se está realizando desde",red+str(100),"hasta",red+str(125))
+            busy = False
+            for subred in range(100, 125):
+                if(int(IP_array[3])!=subred):
+                    print(".", end = "")
+                    direccion = red+str(subred)
+                    response = os.popen(ping()+" "+direccion)
+                    for line in response.readlines():
+                        if ("ttl" in line.lower()):
+                            print()
+                            busy = True
+                            break
+                    if(not busy):
+                        print("\nSe encontró una IP disponible para ser configurada")
+                        staticIP = direccion
+                        break
+                    
+            # Config new static IP on raspberry pi
+            runShellCommand('sudo cp ./src/configFiles/dhcpcd.conf.orig dhcpcd.conf')
+            runShellCommand('sudo python ./src/setStaticIP.py ./dhcpcd.conf {0} {1}'.format(staticIP, Gateway_IP))
+            runShellCommand('sudo cp dhcpcd.conf /etc/dhcpcd.conf')
+            runShellCommand('sudo rm dhcpcd.conf')
+            runShellCommand('sudo systemctl daemon-reload')
+            runShellCommand('sudo systemctl stop dhcpcd.service')
+            for net_dev in os.listdir('/sys/class/net'):
+                runShellCommand('sudo ip addr flush dev {}'.format(net_dev))
+            runShellCommand('sudo systemctl start dhcpcd.service')
+            runShellCommand('sudo systemctl restart networking.service')
+            print("Se configuró este dispósitivo con una IP estática={}".format(staticIP))
+        
+        if(len(staticIP)>3):
+            runShellCommand("sudo python ./src/genID.py config.json ./src/municipios.json {}".format(staticIP))
+        print("\033[0;37;40mSe reiniciará el equipo para que los cambios surtan efecto.")
+        input("Presione enter para finalizar")
+        runShellCommand('sudo reboot')
+        
+    if(attemp==3):
+        print("No esta autorizado para hacer las modificaciones correspondientes. Por favor contacte a su supervisor.")
+        
+else:
+    print("Red WiFi no configurada, por favor configurela para continuar")
---
+++ Python/Master/src/genID.py
@@ -0,0 +1,77 @@
+import sys
+import json
+from collections import OrderedDict
+
+# Functions that generates de unique ID for each container
+def IDgenerator(file, state, city, num):
+    genID= ""
+    stateExist = False
+    cityExist = False
+    
+    with open(file, 'r') as f:
+            data = json.load(f, object_pairs_hook=OrderedDict)
+
+    countState = 1
+    countCity = 1
+
+    for st in data:
+        if(st==state):
+            stateExist = True
+            for ct in data[state]:
+                if(ct==city):
+                    cityExist = True
+                    break
+                else:
+                    countCity += 1
+            break
+        else:
+            countState += 1
+    
+    if(stateExist and cityExist):
+        if(countState<10):
+            genID += "0{}-".format(countState)
+        else:
+            genID += "{}-".format(countState)
+        if(countCity<10):
+            genID += "00{}-".format(countCity)
+        elif(countCity<100):
+            genID += "0{}-".format(countCity)
+        else:
+            genID += "{}-".format(countCity)
+        if(int(num)<10):
+            genID += "00{}".format(num)
+        elif(int(num)<100):
+            genID += "0{}".format(num)
+        else:
+            genID += "{}".format(num)
+        f.close()
+        return genID
+    
+    else:
+        f.close()
+        return None
+
+file = sys.argv[1] # Where ID will save
+jsonFile = sys.argv[2] # Info of states and cities in JSON
+staticIP = sys.argv[3]
+
+# Generate unique ID
+with open(file, 'r') as f:
+    data = json.load(f)
+    state = data['state']
+    city = data['city']
+    num = data['num']
+    genID = IDgenerator(jsonFile, state, city, num)                
+    f.close()
+with open(file, 'w') as f:
+    if(genID!=None):
+        if(len(staticIP)>3):
+            data['staticIP'] = staticIP
+        data['ID'] = genID
+        print("Se generó el ID del sistema={}".format(genID))
+        json.dump(data, f)
+    else:
+        print("\033[1;31;40mError: El ID no sé pudo generar, por favor contacte a su supervisor e informe el error")
+        input("Presione enter para finalizar")
+    f.close()
+
