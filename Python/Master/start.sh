94d402c9a67b243744eb0570704f9925940db308
jmcasimar <chemanuel_95@hotmail.com>
2019-11-29 21:53:00 -0600

    Files to make GrowGreens run as systemd service and start program at boot, reboot and even when program crashed

--- Python/Master/start.sh
+++ Python/Master/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sleep 0
+sleep 1
 export DISPLAY=:0.0
 lxterminal -t "GrowGreens" --working-directory=/home/pi/Documents/GrowGreens/Python/Master --command="/bin/bash -c 'python ./growMaster.py start'"
