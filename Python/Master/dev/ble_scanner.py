ed133036cb0630a8014a642dfa3eed619045f5a7
jmcasimar <chemanuel_95@hotmail.com>
2022-02-25 12:24:58 -0500

    Add BLE basic classes Kalman and Beacon to decodify messages

--- Python/Master/dev/ble_scanner.py
+++ Python/Master/dev/ble_scanner.py
@@ -34,32 +34,63 @@ class Kalman:
         self.p = (1 - self.k) * self.p
         return self.value
 
-def rssi_to_dbm(rssi):
-    return rssi - 256
+class Beacon:
+    def __init__(self, mac, name):
+        self.kalman = Kalman(r=1.4, q=0.065)
+        self.dbm = 0
+        self.distance = 0
+        self.mac = mac
+        self.name = name
 
-# Log Distance Path Loss
-def dbm_to_distance(dbm, rx_power = -75, n=2):
-    # dbm is what we measure
-    # rx_power is dbm measure at 1 meter
-    # n is a constant for the exponential loss. It is calculated from experiments and they are tables to use as reference
-    """
-    Place				    Suggested value
-	Free area  			    	2
-	Urban area 		    		2.7-3.5
-	Suburban area     			3-5
-	Indoor line-of-sight		1.6-1.8
-	Building blocked    		4-6
-	Factory blocked     		2-3
-    """
-    d = pow(10, ((rx_power - dbm) / (10 * n)))
-    return d
+    def rssi_to_dbm(self, rssi):
+        self.dbm = self.kalman.update(rssi) - 256
+        return self.dbm
 
-# David Young´s Distance Algorithm
-def dbm_to_distance_DavidYoung(dbm, rx = -75, tx = -60):
-    # dbm is what we measure
-    # rx is dbm measure at 1 meter
-    # tx is dbm send by BLE
-    d = 1.21112*((dbm * rx/tx)**(7.560861)) + 0.251
+    # Log Distance Path Loss
+    def dbm_to_distance(self, dbm, rx_power = -75, n=2):
+        # dbm is what we measure
+        # rx_power is dbm measure at 1 meter
+        # n is a constant for the exponential loss. It is calculated from experiments and they are tables to use as reference
+        """
+        Place				    Suggested value
+        Free area  			    	2
+        Urban area 		    		2.7-3.5
+        Suburban area     			3-5
+        Indoor line-of-sight		1.6-1.8
+        Building blocked    		4-6
+        Factory blocked     		2-3
+        """
+        d = pow(10, ((rx_power - dbm) / (10 * n)))
+        self.distance = d
+        return d
+
+    # David Young´s Distance Algorithm
+    def dbm_to_distance_DavidYoung(dbm, rx = -75, tx = -60):
+        # dbm is what we measure
+        # rx is dbm measure at 1 meter
+        # tx is dbm send by BLE
+        d = 1.21112*((dbm * rx/tx)**(7.560861)) + 0.251
+        self.distance = d
+        return d
+    
+    # Check Data
+    def check_data(self, data):
+        if self.mac == (':'.join("{0:02x}".format(x) for x in data[12:6:-1])):
+            print("Mac[{}] - Device Name[{}]".format(self.mac, self.name))
+            posNextStructure = 0
+            posBeforeStructure = 0
+            for i, b in enumerate(data):
+                if i == posNextStructure:
+                    print()
+                    posBeforeStructure = i
+                    posNextStructure += int(b)
+                    print("Structure size = ", int(b))
+                elif i == posBeforeStructure: print("Type of data structure is {}".format(b))
+                elif i == len(data) - 1: 
+                    dbm = self.rssi_to_dbm(b)
+                    dist = self.dbm_to_distance(dbm)
+                    print("Intensity: {}, Filter RSSI: {}, Dist: {}".format(b, dbm, dist))
+                else: print("{},".format(b), end = '')
 
 if not os.geteuid() == 0:
     sys.exit("script only works as root")
@@ -105,36 +136,13 @@ if err < 0:
     ))
 
 import time
+bluetoothMac = {"0":{"mac": "ac:23:3f:a9:bf:95", "name": "1"}, "1":{"mac": "ac:23:3f:a9:bf:7c", "name": "2"}, "0":{"mac": "b8:27:eb:ef:a5:87", "name": "rasp"}}
+myBeacons = []
+for dev in bluetoothMac: myBeacons.append(Beacon(dev["mac"], dev["name"]))
+
 while True:
     data = sock.recv(1024)
     # print bluetooth address from LE Advert. packet
     filterMac = ':'.join("{0:02x}".format(x) for x in data[12:6:-1])
-    if filterMac in ["ac:23:3f:a9:bf:95", "ac:23:3f:a9:bf:7c", "b8:27:eb:ef:a5:87", "dc:a6:32:c1:20:46"]:
-        if filterMac=="ac:23:3f:a9:bf:95": print("1 ->", end = '')
-        elif filterMac=="ac:23:3f:a9:bf:7c": print("2 ->", end = '')
-        elif filterMac=="b8:27:eb:ef:a5:87": print("rasp ->", end = '')
-        elif filterMac=="dc:a6:32:c1:20:46": print("local ->", end = '')
-        #print(','.join("{}".format(int(x)) for x in data),end = ' ->')
-        #offset = 3
-        #if(data[0+offset]==0x02): print("data[0]: OK with iBeacon")
-        #if(data[1+offset]==0x01): print("data[1]: OK with iBeacon")
-        #if(data[2+offset]==0x01): print("data[2]: OK with iBeacon")
-        #print("Len of data = {} vs {}".format(data[3+offset], len(data)-offset))
-        #if(data[4+offset:5+offset].hex()==0x4c00): print("APPLE iBeacon")
-        #else: print("Another company iBeacon")
-        
-        #print(data[2+4])
-        #if(data[2+3]==0x06): print("data[2]: OK")
-
-        dbm = rssi_to_dbm(data[-1])
-        dist = dbm_to_distance(dbm)
-        print("RSSI: {}, Dist: {}".format(dbm, dist))
-    """""
-    print(':'.join("{0:02x}".format(x) for x in data),end = '')
-    print(1)
-    for x in data[12:6:-1]:
-        print("{0:02x}".format(x))
-    print(1)
-    time.sleep(10)
-    """""
-    #print(':'.join("{0:02x}".format(x) for x in data[12:6:-1]))
+    if filterMac in [dev.mac for dev in myBeacons]:
+        for dev in myBeacons: myBeacons.check_data(data)
