b16081ef5c6715c45d38e9dce0c3ac26d55d6cf7
jmcasimar <chemanuel_95@hotmail.com>
2022-03-21 12:05:23 -0500

    Debug init

--- Python/PositioningSystem/ble_scanner.py
+++ Python/PositioningSystem/ble_scanner.py
@@ -19,8 +19,8 @@ import utils
 from beacon import Beacon
 from logger import logger
 
-utils.runShellCommand("sudo hciconfig hci0 down")
-utils.runShellCommand("sudo hciconfig hci0 up")
+print(utils.runShellCommand("sudo hciconfig hci0 down"))
+print(utils.runShellCommand("sudo hciconfig hci0 up"))
 
 # Check if temp dir exists, if not then create it
 if not os.path.exists('temp/'): os.makedirs('temp/')
