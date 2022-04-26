0e069f1c52b66728e7f1e41563fb8fae19e2cf40
jmcasimar <chemanuel_95@hotmail.com>
2022-04-26 16:16:33 -0500

    Error in growerStepper.cpp solved

--- C++/libs/GrowerStepper/growerStepper.cpp
+++ C++/libs/GrowerStepper/growerStepper.cpp
@@ -172,7 +172,7 @@ void growerStepper::resetTime()
 bool growerStepper::isTimeToGoHome()
   { if(millis()-__ActualTime>WAIT_TIME_FOR_GO_HOME && !__IsAtHome && !__IsAtHome2){
       printAction(F("Time without move exceed. It is time to go home"), 2);
-      resetTime()
+      resetTime();
       return true;
     }
     else{return false;}
