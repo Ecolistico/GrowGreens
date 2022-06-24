0521e778c62e96996988fd53f01ad8f8f90eadf2
Eleazar <itza.domin@gmail.com>
2022-06-24 11:20:35 -0500

    ManualGrower para debug

---
+++ Python/ManualGrower/moveGrower.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+
+# Import directories
+# Import modules
+import os
+import sys
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+from time import time, sleep, strftime, localtime
+sys.path.insert(0, './src/')
+from gui import GUI
+from mqttControl import mqttClient
+
+def updatePos(gr):
+    if (1<= gr <= 4):
+        publish.single(mqttCloud.pub,
+                        'syncPosCloud,{}'.format(gr),
+                        hostname=mqttCloud.server_ip)
+        mqttCloud.client.loop(0.2)
+        publish.single(mqttCloud.pub,
+                         'syncPosCloud,{}'.format(gr),
+                        hostname=mqttCloud.server_ip)
+        mqttCloud.client.loop(0.2)
+        posX = mqttCloud.X1
+        posY = mqttCloud.Y1
+        gui.updatePos(posX, posY)
+        print("Pos: ",posX, posY)
+
+def createNodos(calX, calY):
+    nodoX = int(calX/41)
+    nodoY = int(calY/10)
+    return nodoX, nodoY
+    
+def moveNodo(nX, nY):
+    pass
+
+def controlDireccion(evento):
+    if evento == "data_up":
+        print("Hola")
+    elif evento == "data_down":
+        print("Hola")
+    elif evento == "data_left":
+        print("Hola")
+    elif evento == "data_right":    
+        print("Hola")
+
+def main():
+    gui = GUI()
+    gui.begin()
+    mqttCloud = mqttClient()
+    # Initialize MQTT connection
+    mqttCloud.mqttInit()
+    gui.updateStatus("Sincroniza un piso(1,3,6,8)")
+    gui.window["data_home"].update(disabled=True)
+    gui.window["data_move"].update(disabled=True)
+    #Sync Here######
+    while gui.isOpen:
+        gui.run()
+        mqttCloud.client.loop(0.05)
+        if gui.sync == True:
+            gui.updateStatus("Sincronizando...")
+            print("Sync....")
+            publish.single(mqttCloud.pub,
+                            'syncCalCloud,{}'.format(gui.gr),
+                            hostname=mqttCloud.server_ip)
+            mqttCloud.client.loop(0.2)
+            publish.single(mqttCloud.pub,
+                            'syncCalCloud,{}'.format(gui.gr),
+                            hostname=mqttCloud.server_ip)
+            mqttCloud.client.loop(0.2)
+            if (1<= gui.gr <= 4):
+                calX = mqttCloud.X1
+                calY = mqttCloud.Y1
+                if (int(calX) != 0 and int(calY) != 0):
+                    print("CalXY Ready: ",calX, calY)
+                    updatePos(gui.gr)
+
+                else:
+                    print("Error, SyncCalXY")
+                    gui.updateStatus("ERROR: SyncCalXY")
+                    break
+                    
+            else:
+                gui.updateStatus("ERROR:Piso Incorrecto")
+                gui.window["data_sincronizar"].update(disabled=True)
+                gui.window["data_home"].update(disabled=True)
+                gui.window["data_move"].update(disabled=True)
+                gui.window["pis"].update(disabled=True)
+                break
+                
+            gui.sync = False
+            gui.window["data_home"].update(disabled=False)
+            gui.window["data_move"].update(disabled=False)
+            #gui.window["data_sincronizar"].update(disabled=True)
+            gui.updateStatus("Ready to go!")
+            print("Sync Ready")
+
+        if gui.home == True:
+            print("Home")
+            gui.updateStatus("Moving To Home..")
+            publish.single(mqttCloud.pub,
+                            'home,{}'.format(gui.gr),
+                            hostname=mqttCloud.server_ip)
+            gui.home = False
+
+        if gui.move == True:
+            if (1<= gui.gr <= 4):
+                #print("Moving to Pos: X:{}, Y:{}")
+                XObj = gui.XObj
+                YObj = gui.YObj
+                print("Move TO: ",XObj, YObj)
+                publish.single(mqttCloud.pub,
+                                'movePosXY,{},{},{}'.format(gui.gr,XObj,YObj),
+                                hostname=mqttCloud.server_ip)
+                gui.move = False
+
+if __name__ == '__main__':
+    main()
---
+++ Python/ManualGrower/src/mqttControl.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+
+# Import directories
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+
+class mqttClient():
+    def __init__(self):
+        #MQTT
+        self.client = mqtt.Client()
+        self.server_ip = 'localhost'
+        
+        self.Topic = "23-009-003/Cloud"
+        self.pub = "23-009-003/Master"
+        
+        self.message = ''
+
+        self.X1 = ''
+        self.Y1 = ''
+        self.CalX1 = ''
+        self.CalY1 = ''
+        self.X2 = ''
+        self.Y2 = ''
+        self.CalX2 = ''
+        self.CalY2 = ''
+        self.X3 = ''
+        self.Y3 = ''
+        self.CalX3 = ''
+        self.CalY3 = ''
+        self.X4 = ''
+        self.Y4 = ''
+        self.CalX4 = ''
+        self.CalY4 = ''
+
+    # MQTT initialize
+    def mqttInit(self):
+        # Reconnect client
+        self.client.on_connect = self.on_connect # Specify on_connect callback
+        self.client.on_message = self.on_message # Specify on_message callback
+        self.client.on_publish = self.on_publish # Specify on_publish callback
+        self.client.on_disconnect = self.on_disconnect # Specify on_disconnect callback
+        
+        # Connect to MQTT Broker Parameters (IP, Port, Seconds Alive)
+        if(self.client.connect(self.server_ip, 1883, 60)==0):
+            pass
+                    
+    # Callback fires when conected to MQTT broker.
+    def on_connect(self, client, userdata, flags, rc):
+        
+        self.messag = "MQTT"
+        
+        if(rc == 0):
+            self.messag += " connection to: " + self.server_ip + " broker, succesful."
+            self.client.subscribe(self.Topic)
+            print(self.messag)
+            print("Subscribed to topic = {}".format(self.Topic))
+            
+        else:
+            self.messag += " Connection to topic: {} is refused".format(self.Topic)
+            if(rc == 1): self.messag += " - incorrect protocol version"
+            elif(rc == 2): self.messag += " - invalid client identifier"
+            elif(rc == 3): self.messag += " - server unavailable"
+            elif(rc == 4): self.messag += " - bad username or password"
+            elif(rc == 5): self.messag += " - not authorised"
+            else: self.messag += " - currently unused"
+            
+            print(self.messag)
+
+    # Callback fires when a published message is received.
+    def on_message(self, client, userdata, msg):    
+        self.top = msg.topic # Input Topic
+        self.device = self.top.split("/")[1] # Device where come
+        
+        self.message = msg.payload.decode("utf-8") # Input message
+        print(self.device + ' has published to ' +
+              self.top + ': ' + self.message)
+        if (self.message.split(",")[0])=="posxy":
+            print('starting SyncPos...')
+            self.X1 = self.message.split(",")[1]
+            self.Y1 = self.message.split(",")[2]
+            #self.CalX1 = self.message.split(",")[1]
+            #self.CalY1 = self.message.split(",")[2]
+            print(self.X1, self.Y1)
+        elif (self.message.split(",")[0])=="sync":
+            print('starting SyncCal...')
+            #self.X1 = self.message.split(",")[1]
+            #self.Y1 = self.message.split(",")[2]
+            self.CalX1 = self.message.split(",")[1]
+            self.CalY1 = self.message.split(",")[2]
+            print(self.CalX1, self.CalY1)
+            
+        else: print(self.message)
+          
+    def on_publish(self, client, userdata, mid):
+        print("Message delivered")
+
+    def on_disconnect(self, client, userdata, rc):
+        print("Client MQTT Disconnected")
+
+def main():
+    cloud = mqttClient()
+    # Initialize MQTT connection
+    cloud.mqttInit()
+    
+    publish.single(cloud.pub,
+                    'Home',
+                    hostname=cloud.server_ip)
+    
+    publish.single(cloud.pub,
+                    'Move',
+                    hostname=cloud.server_ip)
+    
+    while True:
+        if cloud.message == 'closing mqtt':
+            break
+        cloud.client.loop()
+            
+if __name__ == '__main__':
+    main()
+    
