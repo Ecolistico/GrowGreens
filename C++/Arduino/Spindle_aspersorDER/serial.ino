6f370b2af297ced772efc4402e3463bf331c1664
Eleazar <itza.domin@gmail.com>
2022-09-09 06:23:36 -0500

    -Spindle_Aspersor: Serial Functions Added

--- C++/Arduino/Spindle_aspersorDER/serial.ino
+++ C++/Arduino/Spindle_aspersorDER/serial.ino
@@ -5,7 +5,7 @@ void serialEvent() {                                  //if the hardware serial p
   input_string_complete = true;                       //set the flag used to tell if we have received a completed string from the PC
   Serial.println(F("Request accepted"));
   if(input_string_complete==true){
-    String parameter[6];
+    String parameter[7];
     int k = 0;
     int l = 0;
     // Split the parameters
@@ -21,13 +21,25 @@ void serialEvent() {                                  //if the hardware serial p
     }
 
     if(parameter[0]==F("config")){
+      if(parameter[1]==F("all")){
       Serial.print("Saving...");
-      float central = parameter[1].toFloat();
-      float left = parameter[2].toFloat();
-      float right = parameter[3].toFloat();
-      float pressF = parameter[4].toFloat();
-      float pressM = parameter[5].toFloat();
-      write_Flash(central, left, right, pressF, pressM);            
+      float central = parameter[2].toFloat();
+      float left = parameter[3].toFloat();
+      float right = parameter[4].toFloat();
+      float pressF = parameter[5].toFloat();
+      float pressM = parameter[6].toFloat();
+      write_Flash(central, left, right, pressF, pressM);
+      }
+      else {
+      oneParameter(parameter[1], parameter[2].toFloat());
+      }
+    }
+
+    
+
+    if(parameter[0]==F("readConfig")){
+      Serial.print("Reading...");
+      readFlash();
     }
       else{Serial.println(F("error,Spindle(): Parameter[1] does not match a type"));}
     }
